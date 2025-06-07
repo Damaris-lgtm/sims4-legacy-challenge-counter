425d5ce0bcd0d065d96138b0f47d1ac2815aa19a
Damaris <damaris.finger@gmx.de>
2025-06-07 18:12:32 +0200

    Add input for name in sim-view

--- frontend/src/app/components/sims-view/sims-view.component.ts
+++ frontend/src/app/components/sims-view/sims-view.component.ts
@@ -1,27 +1,43 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
 
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { DataStore } from '../../store/data.store';
 import { SimData } from '../../model/generation.model';
 import { TRAITS } from '../../model/traits.data';
 import { Trait } from '../../model/data.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sims-view',
-  imports: [ReactiveFormsModule, MatSelectModule],
+  imports: [ReactiveFormsModule, MatSelectModule, MatInputModule, MatButtonModule],
   templateUrl: './sims-view.component.html',
   styleUrl: './sims-view.component.scss'
 })
 export class SimsViewComponent {
 
   private store = inject(DataStore);
+  protected form = new FormGroup({
+    name: new FormControl<string>('')
+  });
 
   sim = input.required<SimData>();
   TRAITS = TRAITS;
   formTrait = new FormControl<Trait | undefined>(undefined);
+  constructor() {
+    effect(() => {
+      if (this.sim()) {
+        this.form.setValue({
+          name: this.sim().name
+        });
+      }
+    });
+  }
 
   saveSim() {
+    this.sim().name = this.form.value.name || '';
+
     console.log('Saving sim', this.sim());
 
     this.store.updateSim(this.sim());
