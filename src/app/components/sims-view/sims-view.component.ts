81f9b2025c8d920cc6acad6a6a13478a8a6c277b
Damaris <damaris.finger@gmx.de>
2025-06-09 17:00:56 +0200

    add bootstrap columns and add skills

--- src/app/components/sims-view/sims-view.component.ts
+++ src/app/components/sims-view/sims-view.component.ts
@@ -5,7 +5,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { DataStore } from '../../store/data.store';
 import { SimData } from '../../model/generation.model';
 import { TRAITS } from '../../model/traits.data';
-import { Trait, TraitType } from '../../model/data.model';
+import { Skill, Trait, TraitType } from '../../model/data.model';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,6 +14,7 @@ import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/ma
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import {FormsModule} from '@angular/forms';
 import { AchievementSelectionComponent } from "../achievement-selection/achievement-selection.component";
+import { SKILLS } from '../../model/skills.data';
 
 @Component({
   selector: 'app-sims-view',
@@ -28,6 +29,7 @@ export class SimsViewComponent {
 
   sim = input.required<SimData>();
   protected readonly traits = signal(TRAITS);
+  protected readonly skills = signal(SKILLS);
 
   saveSim() {
     this.store.updateSim(this.sim());
@@ -39,6 +41,11 @@ export class SimsViewComponent {
 
   changeTraits(traits: Trait[]) {
     this.sim().traits = traits;
+    this.saveSim();
+  }
+
+  changeSkills(skills: Skill[]) {
+    this.sim().skills = skills;
     this.saveSim();
   }
 
