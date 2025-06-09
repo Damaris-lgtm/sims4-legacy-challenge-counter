810904e3461a5c8bfd4d7b12a408d2cfb5db990d
Damaris <damaris.finger@gmx.de>
2025-06-09 20:18:56 +0200

    add carriers and aspirations to simview

--- src/app/model/carriers.data.ts
+++ src/app/model/carriers.data.ts
@@ -6,216 +6,247 @@ export const CARRIERS: Carrier[] = [
       "label": "Astronaut",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "ATHLETE",
       "label": "Athlet",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "BUSINESS",
       "label": "Geschäft",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "CRIMINAL",
       "label": "Krimineller",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "CULINARY",
       "label": "Kulinarik",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "ENTERTAINER",
       "label": "Entertainer",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "PAINTER",
       "label": "Maler",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "SECRET_AGENT",
       "label": "Geheimagent",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "TECH_GURU",
       "label": "Technikguru",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "WRITER",
       "label": "Schriftsteller",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FULL_TIME
+      "type": CarrierType.FULL_TIME,
+      maxLevel: 10
     },
     {
       "id": "DETECTIVE",
       "label": "Detektiv",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "GET_TO_WORK",
-      "type": CarrierType.ACTIVE
+      "type": CarrierType.ACTIVE,
+      maxLevel: 10
     },
     {
       "id": "DOCTOR",
       "label": "Arzt",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "GET_TO_WORK",
-      "type": CarrierType.ACTIVE
+      "type": CarrierType.ACTIVE,
+      maxLevel: 10
     },
     {
       "id": "SCIENTIST",
       "label": "Wissenschaftler",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "GET_TO_WORK",
-      "type": CarrierType.ACTIVE
+      "type": CarrierType.ACTIVE,
+      maxLevel: 10
     },
     {
       "id": "ACTOR",
       "label": "Schauspieler",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "GET_FAMOUS",
-      "type": CarrierType.ACTIVE
+      "type": CarrierType.ACTIVE,
+      maxLevel: 10
     },
     {
       "id": "INTERIOR_DECORATOR",
       "label": "Innenarchitekt",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "DREAM_HOME_DECORATOR",
-      "type": CarrierType.ACTIVE
+      "type": CarrierType.ACTIVE,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_WRITER",
       "label": "Freiberuflicher Schriftsteller",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_ARTIST",
       "label": "Freiberuflicher Künstler",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_PROGRAMMER",
       "label": "Freiberuflicher Programmierer",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "BASE_GAME",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_PHOTOGRAPHER",
       "label": "Freiberuflicher Fotograf",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "MOSCHINO",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_MAKER",
       "label": "Freiberuflicher Hersteller",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "ECO_LIFESTYLE",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "FREELANCER_PARA_INVESTIGATOR",
       "label": "Paranormaler Ermittler",
       "ages": [Age.YOUNG_ADULT, Age.ADULT, Age.ELDER],
       "pack": "PARANORMAL",
-      "type": CarrierType.FREELANCER
+      "type": CarrierType.FREELANCER,
+      maxLevel: 10
     },
     {
       "id": "BARISTA",
       "label": "Barista",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "BASE_GAME",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "FAST_FOOD_EMPLOYEE",
       "label": "Fast-Food-Mitarbeiter",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "BASE_GAME",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "RETAIL_EMPLOYEE",
       "label": "Einzelhandelsmitarbeiter",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "BASE_GAME",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "MANUAL_LABOR",
       "label": "Manueller Arbeiter",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "BASE_GAME",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "BABYSITTER",
       "label": "Babysitter",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "BASE_GAME",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "LIFEGUARD",
       "label": "Rettungsschwimmer",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "ISLAND_LIVING",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "DIVER",
       "label": "Taucher",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "ISLAND_LIVING",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "FISHERMAN",
       "label": "Fischer",
       "ages": [Age.TEENAGER, Age.YOUNG_ADULT],
       "pack": "ISLAND_LIVING",
-      "type": CarrierType.PART_TIME
+      "type": CarrierType.PART_TIME,
+      maxLevel: 10
     },
     {
       "id": "SCOUT",
       "label": "Pfadfinder",
       "ages": [Age.CHILD, Age.TEENAGER],
       "pack": "SEASONS",
-      "type": CarrierType.CLUB
+      "type": CarrierType.CLUB,
+      maxLevel: 10
     },
     {
       "id": "DRAMA_CLUB",
       "label": "Drama-Club",
       "ages": [Age.CHILD, Age.TEENAGER],
       "pack": "GET_FAMOUS",
-      "type": CarrierType.CLUB
+      "type": CarrierType.CLUB,
+      maxLevel: 10
     }
   ]
