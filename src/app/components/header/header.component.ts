6596edad8a2ade7191950fc3e46905bf23ef6b42
Damaris <damaris.finger@gmx.de>
2025-06-20 22:17:55 +0200

    Move game and rulesetSelection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GameChangeSelectionComponent } from '../generations-overview/game-change-selection/game-change-selection.component';
+import { ResultsSelectionComponent } from '../results/results-selection/results-selection.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, GameChangeSelectionComponent, ResultsSelectionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
